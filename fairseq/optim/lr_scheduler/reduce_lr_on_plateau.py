99dfb3549ba7e07e393d076aee6a83a53149a102
Julien Sfeir <sfeirj@decore0.imag.fr>
2019-04-25 11:10:36 +0200

    modify patience parameter in lr_scheduler

--- fairseq/optim/lr_scheduler/reduce_lr_on_plateau.py
+++ fairseq/optim/lr_scheduler/reduce_lr_on_plateau.py
@@ -22,7 +22,7 @@ class ReduceLROnPlateau(FairseqLRScheduler):
                 ' Consider --lr-scheduler=fixed instead.'
             )
         self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            self.optimizer.optimizer, patience=0, factor=args.lr_shrink)
+            self.optimizer.optimizer, patience=1, factor=args.lr_shrink)
 
     def state_dict(self):
         """Return the LR scheduler state dict."""
